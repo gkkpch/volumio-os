2a9231b23d0f742314721874b89f4a013f15a93f
foonerd <andrew@dt-ltd.net>
2026-01-05 03:16:30 +0000

    feat(pi): enable vc4-kms-v3d for all Pi models via firmware overlay mapping

--- recipes/devices/pi.sh
+++ recipes/devices/pi.sh
@@ -661,11 +661,11 @@ device_chroot_tweaks_pre() {
 		dtoverlay=dwc2,dr_mode=host
 		otg_mode=1
 		[pi5]
-		dtoverlay=vc4-kms-v3d-pi5
 		# dtparam=uart0_console # Disabled by default
 		dtparam=nvme
 		dtparam=pciex1_gen=2
 		[all]
+		dtoverlay=vc4-kms-v3d
 		arm_64bit=0
 		dtparam=audio=on
 		audio_pwm_mode=2
